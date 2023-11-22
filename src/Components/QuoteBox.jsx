9dc4df46aa0120685c28a8fb40f2a12a6d28db7c
Gerardo Merino <gerardosalasmerino100@gmail.com>
2023-11-21 18:33:38 -0600

    project finish

---
+++ src/Components/QuoteBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles/QuoteBox.css';
+
+const QuoteBox = ({ quote, handleChangeQuote }) => {
+
+
+  const { phrase, author } = quote;
+  return (
+    <article className='quotebox'>
+        <h1 className='quotebox__title'>INFOGALAX</h1>
+
+        <div className='quotebox__box'>
+            <p>{phrase}</p>
+        </div>
+
+        <button className='quotebox__btn' onClick={handleChangeQuote}>
+            <img src='/boton.svg' alt='' />
+        </button>
+
+
+        <span className='quotebox__author'>{author}</span>
+        
+    </article>
+  )
+}
+
+export default QuoteBox
